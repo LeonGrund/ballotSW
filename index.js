3630df63901e69d534f380ca1c188c1c98704055
Leon Grund <leoncapgrund@gmail.com>
2018-02-14 09:56:33 -0600

    del cod

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const nonce = require('nonce')();
 const querystring = require('querystring');
 const request = require('request-promise');
 
-const apiKey = process.env.SHOPIFY_API_KEY;
-const apiSecret = process.env.SHOPIFY_API_SECRET;
-const scopes = 'read_products';
-const forwardingAddress = "https://24c7d3bc.ngrok.io"; // Replace this with your HTTPS Forwarding address
+//const apiKey = process.env.SHOPIFY_API_KEY;
+//const apiSecret = process.env.SHOPIFY_API_SECRET;
+//const scopes = 'read_products';
+//const forwardingAddress = "https://24c7d3bc.ngrok.io"; // Replace this with your HTTPS Forwarding address
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!!');
+  res.send('Hello World!');
 });
 
-app.listen(4000, () => {
-  console.log('Example app listening on port 4000!');
+app.listen(8080, () => {
+  console.log('Example app listening on port 8080!');
 });
